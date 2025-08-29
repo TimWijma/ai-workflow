ab13ca7c49ec2db1dc86c503624a194dce137fa6
Tim Wijma <tim.wijma@gmail.com>
2025-08-29 12:27:00 +0200

    delete old frontend

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,8 +1,18 @@
 from .base import Base
 from .session import engine
+from ..models import flow, step, step_connection
 
 def init_db():
+    try:
+        with engine.connect() as connection:
+            print("Successfully connected to the database.")
+    except Exception as e:
+        print(f"Failed to connect to the database: {e}")
+        return
+
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
+    print("Database tables created successfully.")
 
 if __name__ == "__main__":
     init_db()
